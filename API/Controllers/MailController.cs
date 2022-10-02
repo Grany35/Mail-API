b99d35661447c09b9c82a3e5b44e91bafab86889
grany <ogun.ergin35@gmail.com>
2022-10-02 04:46:56 +0300

    Test

---
+++ API/Controllers/MailController.cs
@@ -0,0 +1,38 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> SendMail()
+        {
+            var mail = new MimeMessage();
+            mail.From.Add(MailboxAddress.Parse("admin@ogunergin.com"));
+            mail.To.Add(MailboxAddress.Parse("ogun.ergin35@gmail.com"));
+            mail.Subject = "Test";
+            mail.Body = new TextPart(TextFormat.Html)
+            {
+                Text = "test body"
+            };
+
+            using (var smtp = new SmtpClient())
+            {
+                await smtp.ConnectAsync("smtp.turkticaret.net", 587, SecureSocketOptions.StartTls);
+                await smtp.AuthenticateAsync("admin@ogunergin.com", "PASSWORD GOES HERE");
+                await smtp.SendAsync(mail);
+                await smtp.DisconnectAsync(true);
+
+
+            }
+            return Ok();
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,14 @@
+using Mail.Business.Abstract;
+using Mail.Business.Concrete;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddScoped<IMailService, MailManager>();
+
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
